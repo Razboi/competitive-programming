7ecf432a43b792a6f0b8a5bfc8024f2b6a787805
Marc <recatala.marc@gmail.com>
2019-09-06 18:16:03 +0200

    Solved number_to_string and removed files with old naming convention

--- elixir/even-or-odd.exs
+++
@@ -1,5 +0,0 @@
-defmodule Codewars do
-  def even_or_odd(number) do
-    if rem(number, 2) == 0 do "Even" else "Odd" end
-  end
-end
---
+++ elixir/number_to_string.exs
@@ -0,0 +1,8 @@
+# https://www.codewars.com/kata/5265326f5fda8eb1160004c8
+# We need a function that can transform a number into a string.
+
+defmodule Stringify do
+  def number_to_string(n) do
+    to_string n
+  end
+end
