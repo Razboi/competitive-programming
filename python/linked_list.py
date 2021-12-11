1d761c7a27e6b3f117b9b12ed220315a09df439f
Marc <recatala.marc@gmail.com>
2021-12-11 02:09:00 +0100

    Solved return_kth_to_last in python

--- python/linked_list.py
+++ python/linked_list.py
@@ -4,12 +4,13 @@ class LinkedList:
     def __init__(self, data):
         node = Node(data)
         self.head = node
-        self.count = 0
+        self.count = 1
 
     def add(self, data):
         node = Node(data)
         node.next = self.head
         self.head = node
+        self.count += 1
 
     def print_data(self):
         current_node = self.head
---
+++ python/return_kth_to_last.py
@@ -0,0 +1,36 @@
+from linked_list import LinkedList
+
+# time_complexity: O(n), space_complexity: O(1)
+def return_kth_to_last(list, k):
+    node_index = list.count - k - 1
+    current_node = list.head
+    current_index = 0
+    while current_node.next and current_index != node_index:
+        current_node = current_node.next
+        current_index += 1
+    return current_node
+
+
+# time_complexity: O(n), space_complexity: O(1)
+def return_kth_to_last_without_count(list, k):
+    current_node = list.head
+    runner = list.head
+    for i in range(k):
+        runner = runner.next
+    while runner.next != None:
+        current_node = current_node.next
+        runner = runner.next
+    return current_node
+
+
+
+list = LinkedList(1)
+list.add(2)
+list.add(3)
+list.add(4)
+list.add(5)
+list.add(6)
+list.add(7)
+
+print(return_kth_to_last(list, 3).data)
+print(return_kth_to_last_without_count(list, 3).data)
